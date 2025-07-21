9d637918c8174c7778303750843526ef319c6950
t0fune <106223309+t0fune@users.noreply.github.com>
2025-07-21 13:44:29 +0700

    Create Exercise4_Kornrapart_R.py

---
+++ Exercise4_Kornrapart_R.py
@@ -0,0 +1,12 @@
+
+#Input Score
+FoundationEnglishScore=float(input("Foundation English : "))
+GeneralBusinessScore=float(input("General Business : "))
+IntroductionToComputerSystemsScore=float(input("Introduction to Computer Systems : "))
+ComputerProgrammingScore=float(input("Computer Programming  : "))
+#Output Score
+print("===== Your Score =====")
+print("Foundation English : ",FoundationEnglishScore)
+print("General Business : ",GeneralBusinessScore)
+print("Introduction to Computer Systems : ",IntroductionToComputerSystemsScore)
+print("Computer Programming : ",ComputerProgrammingScore)
